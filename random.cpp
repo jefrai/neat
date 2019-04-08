530afd4e0f75911ec3ab1c1a580fcaf9e1bacb46
jefrai <30693850+jefrai@users.noreply.github.com>
2019-04-09 00:09:49 +0800

    Add files via upload

--- random.cpp
+++ random.cpp
@@ -9,10 +9,10 @@ double sig = 4.9; //sigmoid multiplier
 double cw1 = 1, cw2 = 1, cw3 = 0.4, delt = 3; //excess, disjoint, matching weight, threshold - compatibility constants
 double mut = 0.8, rel = 0.9; //weight mutation rate, relative mutation vs absolute reassignment
 double mug = 0.25, msg = 0.5, muh = 0.7, msh = 0.6; //{intensity, standard deviation} of relative mutation, absolute mutation
-double dsd = 0.75, dtd = 0.05, dte = 0.05; //gene deactivation inheritance, gene deactivation mutation rate, gene activation mutation rate
-double adn = 0.1, adw = 0.1; //edge-split node addition mutation, edge addition mutation
+double dsi = 0.75, dtd = 0.05, dte = 0.05; //gene deactivation inheritance, gene deactivation mutation rate, gene activation mutation rate
+double adn = 0.1, adw = 0.1; //rate of edge-split node addition mutation, edge addition mutation
 double sur = 0.7, isp = 0.005; //proportion of average fitness threshold for survival, interspecies mating rate
-double pur = 0.25; //probability of descent with mutation only without crossover
+double pur = 0.25, pud = 0.1; //probability of descent with mutation absent crossover, descent with crossover absent mutation
 queue<int> q; //heap queue for network query - node identifiers
 map<int, pair<int, double> > m; //heap map for network query - node identifiers to remaining edge dependencies, activation
 map<int, int> rm; //heap map for phenotype node index based on identifier
@@ -39,6 +39,8 @@ struct edg {
         else {if (rdn() < dte) e = 1;}
     }
 };
+
+struct edgcmp {inline bool operator() (const edg& a, const edg& b) {return (a.n < b.n);}};
 
 struct net {
     int L; //layers
@@ -86,6 +88,16 @@ struct net {
         return 0;
     }
 
+    int dme(edg i) { //check if an edge is {disjoint, matched, excess} -> {-1, 0, 1}, sort nodes first
+        int j = lower_bound(edge.begin(), edge.end(), i, edgcmp()) - edge.begin();
+        if (j < edge.size() && i.n == edge[j].n) return 0;
+        j = lower_bound(node.begin(), node.end(), i.a) - node.begin();
+        if (j + 1 > node.size() || i.a != node[j]) return -1;
+        j = lower_bound(node.begin(), node.end(), i.b) - node.begin();
+        if (j + 1 > node.size() || i.b != node[j]) return -1;
+        return 1;
+    }
+
     vector<double> query(vector<double> v) { //run network
         assert(v.size() == n0);
         int i, j;
@@ -124,7 +136,7 @@ net mft(net _n) { //mutate
         ea.b = c;
         ea.e = eg->e;
         ea.w = 1;
-        eb.n = (idl.find(make_pair(c, b)) != idl.end() ? idl[make_pair(c, b)] : idl[make_pair(c, b)] = inw++);
+        ea.n = (idl.find(make_pair(c, b)) != idl.end() ? idl[make_pair(c, b)] : idl[make_pair(c, b)] = inw++);
         eb.a = c;
         eb.b = b;
         eb.e = eg->e;
@@ -144,10 +156,21 @@ net mft(net _n) { //mutate
         eg.e = 1;
         eg.w = (rdn() * 2 - 1) * muh;
     }
+    return n;
 }
 
 net ncr(net a, net b) { //crossbreed
-
+    int i, j;
+    if (a.F < b.F) swap(a, b);
+    for (i = j = 0; i < a.edge.size(); ++i) {
+        while (j < b.edge.size() && b.edge[j].n < a.edge[i].n) ++j;
+        if (a.edge[i].n == b.edge[i].n) {
+            a.edge[i].w = rng(2) ? b.edge[j].w : a.edge[i].w;
+            a.edge[i].e = (a.edge[i].e && b.edge[i].e) ? 1 : (rdn() > dsi);
+        }
+    }
+    if (rdn() > pud) a = mft(a);
+    return a;
 }
 
 struct spec {
@@ -163,6 +186,28 @@ struct spec {
         while (spr.pop.size() > m) spr.pop.pop_back();
         return spr;
     }
+
+    static bool eq(net a, net b) { //check for same species
+        int c, mt = 0, i;
+        double dst = 0, mdw = 0;
+        if (a.edge.size() < b.edge.size()) swap(a, b);
+        sort(a.node.begin(), a.node.end());
+        sort(b.node.begin(), b.node.end());
+        for (i = 0; i < a.edge.size(); ++i) {
+            c = b.dme(a.edge[i]);
+            if (c == -1) dst += cw2;
+            if (!c) {++mt; mdw += fabs(a.edge[i].w - lower_bound(b.edge.begin(), b.edge.end(), a.edge[i], edgcmp())->w);}
+            if (c == 1) dst += cw1;
+        }
+        for (i = 0; i < b.edge.size(); ++i) {
+            c = a.dme(b.edge[i]);
+            if (c == -1) dst += cw2;
+            if (c == 1) dst += cw1;
+        }
+        dst /= a.edge.size();
+        dst += mdw * cw3 / mt;
+        return dst < delt;
+    }
 };
 
 vector<net> ppn[2]; //population
@@ -176,7 +221,7 @@ inline void init() {
 }
 
 inline void epoch(int tI) {
-    //TODO EVALUATE NETS
+    //TODO EVALUATE NETS + SPECIES AUGMENTATION
     //TODO SURVIVAL & ELIMINATION
     //TODO REGEN POPULATION
     //TODO REFILL SPECIES
